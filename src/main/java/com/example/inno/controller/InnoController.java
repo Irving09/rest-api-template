708d524b1abd1e2f2fbadf7ecc194ce6ec88c057
Inno Estrera <inno.estrera@cdk.com>
2017-12-28 14:21:51 -0800

    fixed request mapping for controller

--- src/main/java/com/example/inno/controller/InnoController.java
+++ src/main/java/com/example/inno/controller/InnoController.java
@@ -11,17 +11,33 @@ package com.example.inno.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 /**
  * @author irving09 <innoirvinge@gmail.com>
  */
-@RestController("v1/inno")
+@RestController
+@RequestMapping("/v1/inno")
 public class InnoController {
 
-    @GetMapping
+    @GetMapping(
+        value = "/test1",
+        produces = APPLICATION_JSON_VALUE)
+    @ResponseBody
     public ResponseEntity<String> test() {
         return ResponseEntity.ok("{ message: \"hello world\"}");
+    }
+
+    @GetMapping(
+        value = "/test2",
+        produces = APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseEntity<String> test2() {
+        return ResponseEntity.ok("{ message: \"hello world 2\"}");
     }
 
 }
