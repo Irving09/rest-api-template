ce978aefb445389a0b9d6c92c18bfca6bdf4bc18
Inno Estrera <inno.estrera@cdk.com>
2017-12-28 14:25:25 -0800

    added swagger config

--- src/main/java/com/example/inno/config/SwaggerConfig.java
+++ src/main/java/com/example/inno/config/SwaggerConfig.java
@@ -7,10 +7,27 @@
  * Date: Dec 28, 2017
  * Copyright 2017 innoirvinge@gmail.com
  */
-package com.example.inno.configuration;
+package com.example.inno.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author irving09 <innoirvinge@gmail.com>
  */
+@Configuration
+@EnableSwagger2
 public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any()).build();
+    }
 }
