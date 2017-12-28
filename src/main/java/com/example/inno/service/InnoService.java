ce6ddd269579e9a257c5764cceb40706beec5197
Inno Estrera <inno.estrera@cdk.com>
2017-12-28 15:55:28 -0800

    Added service layer with mock data

---
+++ src/main/java/com/example/inno/service/InnoService.java
@@ -0,0 +1,16 @@
+/**
+ * CONFIDENTIAL INFORMATION
+ * <p>
+ * All Rights Reserved.  Unauthorized reproduction, transmission, or
+ * distribution of this software is a violation of applicable laws.
+ * <p>
+ * Date: Dec 28, 2017
+ * Copyright 2017 innoirvinge@gmail.com
+ */
+package com.example.inno.service;
+
+/**
+ * @author irving09 <innoirvinge@gmail.com>
+ */
+public class InnoService {
+}
